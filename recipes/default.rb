7bdc1292b5b5c0c06b812680527736a7f67e29d5
pauldambra <pauldambra@gmail.com>
2016-07-30 16:54:17 +0100

    add git config commands

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,10 @@ include_recipe 'homebrew'
 git_user_cmd = "git config --global user.name #{node.default['user']['name']}"
 git_email_cmd = "git config --global user.email \"#{node.default['user']['name']}@gmail.com\""
 git_autocorrect_cmd = 'git config --global help.autocorrect 5'
+git_push_cmd = "git config --global push.default simple"
 
 execute "config git" do
-  command "#{git_user_cmd} && #{git_email_cmd} && #{git_autocorrect_cmd}"
+  command "#{git_user_cmd} && #{git_email_cmd} && #{git_autocorrect_cmd} && #{git_push_cmd}"
   user node.default['user']['name']
 end
 
