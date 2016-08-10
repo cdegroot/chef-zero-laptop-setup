1de1f24df479cc285712dfda0e11607f1ebfe1da
pauldambra <pauldambra@gmail.com>
2016-08-10 22:56:58 +0100

    fix the incorrect syntax used for setting up a symbolic link

--- recipes/dotNetCore.rb
+++ recipes/dotNetCore.rb
@@ -5,31 +5,20 @@
 # ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
 
 package 'openssl' do
-	version '1.0.2h_1'
-	notifies :create, 'link[libcrypto]', :immediately
-	notifies :create, 'link[libssl]', :immediately
-	notifies :create, 'link[openssl]', :immediately
 end
 
-link 'openssl' do
-  target_file '/usr/local/Cellar/openssl/1.0.2h_1/bin/openssl'
-  to '/usr/local/bin/'
+link '/usr/local/lib/libcrypto.1.0.0.dylib' do
+  to '/usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib'
   link_type :symbolic
   action :nothing
+  subscribes :create, 'package[openssl]', :immediately
 end
 
-link 'libcrypto' do
-  target_file '/usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib'
-  to '/usr/local/lib/'
+link '/usr/local/lib/libssl.1.0.0.dylib' do
+  to '/usr/local/opt/openssl/lib/libssl.1.0.0.dylib'
   link_type :symbolic
   action :nothing
-end
-
-link 'libssl' do
-  target_file '/usr/local/opt/openssl/lib/libssl.1.0.0.dylib'
-  to '/usr/local/lib/'
-  link_type :symbolic
-  action :nothing
+  subscribes :create, 'package[openssl]', :immediately
 end
 
 dotnet_file = "#{Chef::Config[:file_cache_path]}/dotnetcore.pkg"
