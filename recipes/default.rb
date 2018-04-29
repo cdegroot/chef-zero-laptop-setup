c1b0b4686e1b65f0509ac0ea923c4db6aaa82281
Cees de Groot <casedeg@gmail.com>
2018-04-28 23:27:39 -0400

    Add some more stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,8 @@
 
 include_recipe 'devbox::git'
 include_recipe 'devbox::asdf'
+include_recipe 'devbox::emacs'
+include_recipe 'devbox::vim'
 
 # Todo:
-# Setup spacemacs
-# Setup vimrc
 # Setup profile/bash
