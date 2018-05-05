b92f12cde4ad709c435d2e6247f8bca072bc7f31
Cees de Groot <casedeg@gmail.com>
2018-05-04 22:38:30 -0400

    Revendor everything

--- cookbooks/devbox/recipes/terminator.rb
+++ cookbooks/devbox/recipes/terminator.rb
@@ -1,6 +1,11 @@
 package 'terminator' do
   action :upgrade
 end
+directory "#{node.default['user']['homedir']}/.config/terminator" do
+  mode '0755'
+  owner node.default['user']['account']
+  recursive true
+end
 cookbook_file "#{node.default['user']['homedir']}/.config/terminator/config" do
   source 'terminator-config'
   owner node.default['user']['account']
