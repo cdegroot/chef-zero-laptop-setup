21bb9ed8b97a796fe39506c7a11804c26bc864ec
Cees de Groot <casedeg@gmail.com>
2018-05-04 22:41:56 -0400

    Try fix nerves by adding homedir

--- cookbooks/devbox/recipes/nerves.rb
+++ cookbooks/devbox/recipes/nerves.rb
@@ -6,5 +6,8 @@ end
 execute "mix archive.install hex nerves_bootstrap 1.0.0 --force" do
   user node.default['user']['account']
   cwd  node.default['user']['homedir']
+  environment ({
+    'HOME' => node.default['user']['homedir']
+  })
   creates "#{node.default['user']['homedir']}/.asdf/installs/elixir/1.6.4/.mix/archives/nerves_bootstrap-1.0.0"
 end
