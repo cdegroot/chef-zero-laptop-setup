26dc1f6f28514b8e8c5b5bd042da1781847a63ba
Cees de Groot <casedeg@gmail.com>
2018-05-09 09:39:22 -0400

    Add some todos

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,10 @@
 home = node.default['user']['homedir']
 ENV['PATH'] = "#{ENV['PATH']}:" + "#{home}/.asdf/bin:#{home}/.asdf/shims"
 
+package 'nano' do
+  action :remove
+end
+
 include_recipe 'devbox::git'
 include_recipe 'devbox::asdf'
 include_recipe 'devbox::emacs'
@@ -12,4 +16,10 @@ include_recipe 'devbox::terminator'
 include_recipe 'devbox::nerves'
 
 # Todo:
-# Setup profile/bash
+# - Add private key that can talk to github for at least checkout. The idea is
+# to add a dummy user and add that as collaborator r/o on private repos, than
+# have that ssh key in this repo. Not ideal, but doable. Alternatively, research
+# Chef encrypted databags.
+# - Setup profile/bash, dotfiles repo
+# - Integrate ~/.spacemacs.d with dotfiles repo
+# - Install syncthing(?).
